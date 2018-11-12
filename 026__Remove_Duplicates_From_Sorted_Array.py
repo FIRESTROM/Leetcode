cd366bddcb41682420fe35c3ec7647a688146189
Junou Cui <junou_cui@berkeley.edu>
2018-11-12 15:26:09 -0800

    02

---
+++ 026__Remove_Duplicates_From_Sorted_Array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        index = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[index] = nums[i]
+                index += 1
+        return index
+        
