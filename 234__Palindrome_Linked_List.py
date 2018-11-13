1eaa19234724a5be804f248f861b651099e72889
Junou Cui <junou_cui@berkeley.edu>
2018-11-13 15:31:18 -0800

    234

---
+++ 234__Palindrome_Linked_List.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        result = []
+        while head:
+            result.append(head.val)
+            head=head.next
+        if result == result[::-1]:
+            return True
+        return False
+        
