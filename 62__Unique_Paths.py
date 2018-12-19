2daa8433ce60208454481bd73c030fb4724930bf
Junou Cui <junou_cui@berkeley.edu>
2018-12-18 19:24:42 -0800

    062

---
+++ 62__Unique_Paths.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[1] * m for _ in range(n)]
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[-1][-1]
+        
