5471f42b34ca709d48db9f0943aa52c6233e3738
Junou Cui <junou_cui@berkeley.edu>
2018-12-19 20:09:21 -0800

    034

--- 034__Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ 034__Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -1,29 +1,33 @@
 class Solution(object):
-
-    def extreme_insertion_index(self, nums, target, left):
-        l = 0
-        r = len(nums)
-
-        while l < r:
-            mid = (l + r) // 2
-            if nums[mid] > target or (left and target == nums[mid]):
-                r = mid
-            else:
-                l = mid + 1
-
-        return l
-
     def searchRange(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        left_idx = self.extreme_insertion_index(nums, target, True)
+        def findmax(l, r):
+            while l < r:
+                mid = (l + r) // 2
+                if nums[mid] <= target:
+                    l = mid + 1
+                else:
+                    r = mid
+            return l - 1
 
-        # assert that `left_idx` is within the array bounds and that `target`
-        # is actually in `nums`.
-        if left_idx == len(nums) or nums[left_idx] != target:
+        def findmin(l, r):
+            while l < r:
+                mid = (l + r) // 2
+                if nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid
+            return l
+
+        if len(nums) == 0:
             return [-1, -1]
 
-        return [left_idx, self.extreme_insertion_index(nums, target, False) - 1]
+        index_l = findmin(0, len(nums))
+        if index_l == len(nums) or nums[index_l] != target:
+            return [-1, -1]
+
+        return [index_l, findmax(0, len(nums))]
