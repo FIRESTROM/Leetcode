22ef698f3a06b375f1ac05f1986eec07bfe93c9c
Junou Cui <junou_cui@berkeley.edu>
2018-12-20 16:35:51 -0800

    070

---
+++ 070__Climbing_Stairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+
+        dp = [0 for _ in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i - 2] + dp[i - 1]
+        return dp[-1]
