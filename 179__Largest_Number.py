351b4551b14244214d46c82468c28a3909d448e8
Junou Cui <junou_cui@berkeley.edu>
2018-12-21 21:35:19 -0800

    179

--- 179__Largest_Number.py
+++ 179__Largest_Number.py
@@ -8,3 +8,13 @@ class Solution(object):
         nums = map(str,nums)
         nums.sort(cmp = comp, reverse = True)
         return str(int("".join(nums)))
+
+#  we define a function that compares two string (a,b) . we consider a bigger than b if a+b>b+a
+#  for example : (a="2",b="11") a is bigger than b because "211" >"112"
+
+#  convert all elements of the list from int to string
+
+#  sort the list descendingly using the comparing function we defined
+#  for example sorting this list ["2","11","13"] using the function defined in step 1 would produce ["2","13","11"]
+
+#  we concatatenate the list "21311"
