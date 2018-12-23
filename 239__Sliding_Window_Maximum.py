fa83ded313d6ddc9c841a7a2832840b0f2ca17cb
Junou Cui <junou_cui@berkeley.edu>
2018-12-23 14:37:55 -0800

    239

---
+++ 239__Sliding_Window_Maximum.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if k > nums:
+            return [max(nums)]
+        if len(nums) == 0:
+            return []
+        temp_max = nums[0]
+        valid = 0
+        for i in range(k):
+            if nums[i] > temp_max:
+                temp_max = nums[i]
+                valid = i
+        print(valid)
+        result = [temp_max]
+        for i in range(1, len(nums) - k + 1):
+            if valid == 0:
+                temp_max, valid = nums[i], 0
+                for j in range(i, i + k):
+                    if nums[j] > temp_max:
+                        temp_max = nums[j]
+                        valid = j - i
+                result.append(temp_max)
+            else:
+                if temp_max > nums[i + k - 1]:
+                    result.append(temp_max)
+                    valid -= 1
+                else:
+                    temp_max = nums[i + k - 1]
+                    result.append(temp_max)
+                    valid = k - 1
+        return result
