e83672cc5678fabf067525c3b61a53990adcfe55
Junou Cui <junou_cui@berkeley.edu>
2018-12-26 16:50:57 -0800

    063

---
+++ 063__Unique_Paths_II.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        obstacleGrid[0][0] = 1
+
+        for i in range(1,m):
+            obstacleGrid[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 1)
+
+        for j in range(1, n):
+            obstacleGrid[0][j] = int(obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 1)
+
+        for i in range(1,m):
+            for j in range(1,n):
+                if obstacleGrid[i][j] == 0:
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+                else:
+                    obstacleGrid[i][j] = 0
+
+        return obstacleGrid[-1][-1]
