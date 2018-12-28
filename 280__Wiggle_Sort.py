d8a0cd767a5437ecaf5a9157ef456f3eb4fccc73
Junou Cui <junou_cui@berkeley.edu>
2018-12-27 19:19:51 -0800

    280

---
+++ 280__Wiggle_Sort.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        check = True
+        for i in range(len(nums) - 1):
+            if check:
+                if nums[i] > nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            else:
+                if nums[i] < nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            check = not check
