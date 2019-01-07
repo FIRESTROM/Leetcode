5b5d4bdae4e2136af9ed2254ea6152b7299937ac
Junou Cui <junou_cui@berkeley.edu>
2019-01-06 16:26:01 -0800

    374

---
+++ 374__Guess_Number_Higher_or_Lower.py
@@ -0,0 +1,22 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left < right:
+            mid = (left + right) / 2
+            check = guess(mid)
+            if check == 0:
+                return mid
+            elif check == 1:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
