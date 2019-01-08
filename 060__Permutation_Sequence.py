9b4d280e496ac3a50dd4f2f5c383da4f94e36d31
Junou Cui <junou_cui@berkeley.edu>
2019-01-07 19:30:59 -0800

    060

---
+++ 060__Permutation_Sequence.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        numbers = range(1, n + 1)
+        permutation = ""
+        k -= 1
+        while n > 0:
+            n -= 1
+            # get the index of current digit
+            index, k = divmod(k, math.factorial(n))
+            permutation += str(numbers[index])
+            # remove handled number
+            numbers.remove(numbers[index])
+
+        return permutation
