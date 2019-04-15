aabbc217d4bcc05bf9f866cdd18b72649c027e86
Junou Cui <junou_cui@berkeley.edu>
2019-04-15 11:25:08 -0700

    085

---
+++ Java/085__Maximal_Rectangle.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] height = new int[n];
+        Arrays.fill(right, n);
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            int cur_left = 0, cur_right = n;
+            for (int j = 0; j < n; j++) {
+                // Updare height
+                if (matrix[i][j] == '1') {
+                    height[j]++;
+                } else {
+                    height[j] = 0;
+                }
+                // Update left
+                if (matrix[i][j] == '1') {
+                    left[j] = Math.max(left[j], cur_left);
+                } else {
+                    left[j] = 0;
+                    cur_left = j + 1;
+                }
+            }
+            // Update right
+            for (int j = n - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    right[j] = Math.min(right[j], cur_right);
+                } else {
+                    right[j] = n;
+                    cur_right = j;
+                }
+            }
+            for(int j = 0; j < n; j++) {
+                result = Math.max(result, (right[j] - left[j]) * height[j]);
+            }
+        }
+        return result;
+    }
+}
