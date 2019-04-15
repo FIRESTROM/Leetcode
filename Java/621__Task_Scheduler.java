9ac43b843f086c1ef9b7be5342fd8c29cb802676
Junou Cui <junou_cui@berkeley.edu>
2019-04-15 15:50:47 -0700

    621

---
+++ Java/621__Task_Scheduler.java
@@ -0,0 +1,61 @@
+// Using Sort
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char c : tasks) {
+            map[c - 'A']++;
+        }
+        Arrays.sort(map);
+        int time = 0;
+        while (map[25] > 0) {
+            int i = 0;
+            while (i <= n) {
+                if (map[25] == 0) {
+                    break;
+                }
+                if (i < 26 && map[25 - i] > 0) {
+                    map[25 - i]--;
+                }
+                time++;
+                i++;
+            }
+            Arrays.sort(map);
+        }
+        return time;
+    }
+}
+
+// Using Priority-Queue
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char c: tasks)
+            map[c - 'A']++;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(26, Collections.reverseOrder());
+        for (int f: map) {
+            if (f > 0)
+                queue.add(f);
+        }
+        int time = 0;
+        while (!queue.isEmpty()) {
+            int i = 0;
+            List<Integer> temp = new ArrayList<>();
+            while (i <= n) {
+                if (!queue.isEmpty()) {
+                    if (queue.peek() == 1) {
+                        queue.poll();
+                    } else {
+                        temp.add(queue.poll() - 1);
+                    }
+                }
+                time++;
+                if (queue.isEmpty() && temp.size() == 0)
+                    break;
+                i++;
+            }
+            for (int l: temp)
+                queue.add(l);
+        }
+        return time;
+    }
+}
