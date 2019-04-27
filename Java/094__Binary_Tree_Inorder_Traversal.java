0ee24b0a3da47e0d2d2a2c7c7298f7037292d5d2
Junou Cui <junou_cui@berkeley.edu>
2019-04-26 21:35:46 -0700

    094

---
+++ Java/094__Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(result, root);
+        return result;
+    }
+
+    private void helper(List<Integer> result, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        helper(result, root.left);
+        result.add(root.val);
+        helper(result, root.right);
+    }
+}
+
+
+// Another Solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List < Integer > result = new ArrayList < > ();
+        Stack < TreeNode > stack = new Stack < > ();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            result.add(curr.val);
+            curr = curr.right;
+        }
+        return result;
+    }
+
+}
