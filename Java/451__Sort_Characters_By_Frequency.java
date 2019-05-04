7eb9a803ac3cb9003a410dd5308c8ac2da9cf298
Junou Cui <junou_cui@berkeley.edu>
2019-05-03 21:43:21 -0700

    451

---
+++ Java/451__Sort_Characters_By_Frequency.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> map = new HashMap();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, (o1, o2) -> (o2.getValue()).compareTo(o1.getValue()));
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<Character, Integer> entry : list) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                stringBuilder.append(entry.getKey());
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
