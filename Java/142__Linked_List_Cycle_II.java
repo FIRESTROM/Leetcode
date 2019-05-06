10ee19a19bc61afc2a3ddc3372301515b500c5c3
Junou Cui <junou_cui@berkeley.edu>
2019-05-05 18:47:49 -0700

    142

---
+++ Java/142__Linked_List_Cycle_II.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode intersect = getIntersect(head);
+        if (intersect == null) {
+            return null;
+        }
+        ListNode ptr1 = head, ptr2 = intersect;
+        while (ptr1 != ptr2) {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+        return ptr1;
+
+    }
+
+    private ListNode getIntersect(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return slow;
+            }
+        }
+        return null;
+    }
+}
