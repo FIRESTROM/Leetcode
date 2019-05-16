8cd931f27f8c612ba5428e2d68d0da8e056ee9be
Junou Cui <junou_cui@berkeley.edu>
2019-05-16 11:11:14 -0700

    487

--- Java/487__Max_Consecutive_Ones_II.java
+++ Java/487__Max_Consecutive_Ones_II.java
@@ -1,5 +1,27 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
+        int result = 0;
+        int prev = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) count++;
+            else if (prev == -1) {
+                prev = i;
+                count++;
+            } else {
+                count = i - prev;
+                prev = i;
+            }
+            result = Math.max(result, count);
+        }
+        return result;
+    }
+}
+
+
+// Another Solution
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
         int flippedCounter = -1;
         int count = 0;
         int result = 0;
