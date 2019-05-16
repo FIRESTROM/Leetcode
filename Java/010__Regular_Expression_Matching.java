f8a49d05d6f05df0ff31aca02b74fdefb3ac4e83
Junou Cui <junou_cui@berkeley.edu>
2019-05-16 14:03:33 -0700

    010

---
+++ Java/010__Regular_Expression_Matching.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+
+        if (s == null || p == null) return false;
+
+        boolean [][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int j = 1; j < p.length(); j++) {
+            if (p.charAt(j) == '*') dp[0][j + 1] = dp[0][j - 1];
+        }
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.') dp[i + 1][j + 1] = dp[i][j];
+                if (p.charAt(j) == s.charAt(i)) dp[i + 1][j + 1] = dp[i][j];
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
