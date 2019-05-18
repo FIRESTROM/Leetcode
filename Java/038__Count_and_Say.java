679e43831b985d0300c6f22abacd0a30db5ba77b
Junou Cui <junou_cui@berkeley.edu>
2019-05-17 19:55:17 -0700

    093

--- Java/038__Count_and_Say.java
+++ Java/038__Count_and_Say.java
@@ -10,16 +10,16 @@ class Solution {
 
     private String helper(String s) {
         int count = 1;
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) == s.charAt(i - 1)) {
                 count++;
             } else {
-                result += String.valueOf(count) + Character.toString(s.charAt(i - 1));
+                result.append(count).append(s.charAt(i - 1));
                 count = 1;
             }
         }
-        result += String.valueOf(count) + Character.toString(s.charAt(s.length() - 1));
-        return result;
+        result.append(count).append(s.charAt(s.length() - 1));
+        return result.toString();
     }
 }
