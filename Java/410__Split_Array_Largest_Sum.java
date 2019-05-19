de7d57d46332a79a79cb75e474f4a61ea58533d6
Junou Cui <junou_cui@berkeley.edu>
2019-05-19 12:36:02 -0700

    410

---
+++ Java/410__Split_Array_Largest_Sum.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int n = nums.length;
+        int[][] dp = new int[n + 1][m + 1];
+        int[] sub = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            sub[i + 1] = sub[i] + nums[i];
+        }
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                for (int k = 0; k < i; k++) {
+                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[k][j - 1], sub[i] - sub[k]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
+
+// Binary Search Solution
+
+public class Solution {
+    public int splitArray(int[] nums, int m) {
+        int max = 0; long sum = 0;
+        for (int num : nums) {
+            max = Math.max(num, max);
+            sum += num;
+        }
+        if (m == 1) return (int)sum;
+        //binary search
+        long l = max; long r = sum;
+        while (l <= r) {
+            long mid = (l + r)/ 2;
+            if (valid(mid, nums, m)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int)l;
+    }
+    public boolean valid(long target, int[] nums, int m) {
+        int count = 1;
+        long total = 0;
+        for(int num : nums) {
+            total += num;
+            if (total > target) {
+                total = num;
+                count++;
+                if (count > m) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
