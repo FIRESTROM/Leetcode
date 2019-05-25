3515db8ba7325d716e4eea98b0377a12d4f68fc9
Junou Cui <junou_cui@berkeley.edu>
2019-05-25 15:52:00 -0700

    309

---
+++ Java/309__Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        if (prices == null || prices.length == 0)
+            return 0;
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for (int i =1 ; i < prices.length; i++) {
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            dp[i][0] = Math.max(dp[i - 1][0], (i > 2 ? dp[i - 2][1] : 0) - prices[i]);
+        }
+        return dp[prices.length - 1][1];
+    }
+}
