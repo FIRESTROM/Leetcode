2a7c9943076b4a39f1980234544b0dd7dbbece50
Junou Cui <junou_cui@berkeley.edu>
2019-05-27 12:47:27 -0700

    316

---
+++ Java/316__Remove_Duplicate_Letters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        if (s == null)
+            return null;
+        StringBuilder result = new StringBuilder();
+        int[] count = new int[26];
+        boolean[] actual = new boolean[26];
+        char[] s_arr = s.toCharArray();
+        for (int index = 0 ; index < actual.length ; index++)
+            actual[index] = false;
+        for (char c : s_arr)
+            count[c - 'a']++;
+        for( char c : s_arr) {
+            int length = result.length();
+            while (length > 0 && c < result.charAt(length-1) && count[result.charAt(length - 1) - 'a'] > 1 && !actual[c - 'a']) {
+                count[result.charAt(length - 1) - 'a']--;
+                actual[result.charAt(length - 1) - 'a'] = false;
+                result.deleteCharAt(length - 1);
+                length = result.length();
+            }
+            if (!actual[c - 'a']) {
+                result.append(c);
+                actual[c - 'a'] = true;
+            }else
+                count[c - 'a']--;
+        }
+        return result.toString();
+    }
+}
