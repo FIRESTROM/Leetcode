40181f786a7cecfc6a7264a873ae70b7322a7538
Junou Cui <junou_cui@berkeley.edu>
2019-06-03 20:14:41 -0700

    364

---
+++ Java/364__Nested_List_Weight_Sum_II.java
@@ -0,0 +1,58 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+
+    private int total_depth = 1;
+
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        count(nestedList, total_depth); // count the total level
+        return partialSum(nestedList, total_depth);
+    }
+
+    private void count(List<NestedInteger> nestedList, int depth){
+        total_depth = Math.max(depth, total_depth);
+        for (int i = 0; i < nestedList.size(); i++) {
+            NestedInteger curNI = nestedList.get(i);
+            if (!curNI.isInteger())
+                count(curNI.getList(), depth + 1);
+        }
+    }
+
+    private int partialSum(List<NestedInteger> nestedList, int depth){
+        int count = 0;
+        for (int i = 0; i < nestedList.size(); i++) {
+            NestedInteger curNI = nestedList.get(i);
+            if (curNI.isInteger())
+                count += depth * curNI.getInteger();
+            else
+                count += partialSum(curNI.getList(), depth - 1);
+        }
+        return count;
+    }
+}
