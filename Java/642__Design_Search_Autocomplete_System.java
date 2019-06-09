8e8e4ae9500156fc9fed576557c36c72f169db70
Junou Cui <junou_cui@berkeley.edu>
2019-06-08 17:52:34 -0700

    642

---
+++ Java/642__Design_Search_Autocomplete_System.java
@@ -0,0 +1,75 @@
+
+public class AutocompleteSystem {
+
+    class Node {
+        String sentence;
+        int times;
+        Node (String st, int t) {
+            sentence = st;
+            times = t;
+        }
+    }
+
+    class Trie {
+        int times;
+        Trie[] branches = new Trie[27];
+    }
+
+    Trie root;
+    String cur_sent = "";
+
+    public AutocompleteSystem(String[] sentences, int[] times) {
+        root = new Trie();
+        for (int i = 0; i < sentences.length; i++)
+            insert(root, sentences[i], times[i]);
+    }
+
+    public List<String> input(char c) {
+        List<String> result = new ArrayList<>();
+        if (c == '#') {
+            insert(root, cur_sent, 1);
+            cur_sent = "";
+        } else {
+            cur_sent += c;
+            List < Node > list = lookup(root, cur_sent);
+            Collections.sort(list, (a, b) -> a.times == b.times ? a.sentence.compareTo(b.sentence) : b.times - a.times);
+            for (int i = 0; i < Math.min(3, list.size()); i++)
+                result.add(list.get(i).sentence);
+        }
+        return result;
+    }
+
+    private void insert(Trie t, String s, int times) {
+        for (int i = 0; i < s.length(); i++) {
+            if (t.branches[int_(s.charAt(i))] == null)
+                t.branches[int_(s.charAt(i))] = new Trie();
+            t = t.branches[int_(s.charAt(i))];
+        }
+        t.times += times;
+    }
+
+    private List<Node> lookup(Trie t, String s) {
+        List<Node> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (t.branches[int_(s.charAt(i))] == null)
+                return new ArrayList<>();
+            t = t.branches[int_(s.charAt(i))];
+        }
+        traverse(s, t, list);
+        return list;
+    }
+
+    private void traverse(String s, Trie t, List < Node > list) {
+        if (t.times > 0)
+            list.add(new Node(s, t.times));
+        for (char i = 'a'; i <= 'z'; i++)
+            if (t.branches[i - 'a'] != null)
+                traverse(s + i, t.branches[i - 'a'], list);
+        if (t.branches[26] != null)
+            traverse(s + ' ', t.branches[26], list);
+    }
+
+    private int int_(char c) {
+        return c == ' ' ? 26 : c - 'a';
+    }
+}       
