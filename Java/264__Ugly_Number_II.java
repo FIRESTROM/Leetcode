0e6901f39877094c66d018aaf1e17917a7067028
Junou Cui <junou_cui@berkeley.edu>
2019-07-05 20:14:59 -0700

    264

---
+++ Java/264__Ugly_Number_II.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        if (n < 0) return -1;
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        for (int i = 1; i < n; i++) {
+            ugly[i] = Math.min(ugly[i2] * 2, Math.min(ugly[i3] * 3, ugly[i5] * 5));
+            if (ugly[i] == ugly[i2] * 2) i2++;
+            if (ugly[i] == ugly[i3] * 3) i3++;
+            if (ugly[i] == ugly[i5] * 5) i5++;
+        }
+        return ugly[n - 1];
+    }
+}
