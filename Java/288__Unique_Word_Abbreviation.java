94f4fa03126180e7f1654fd8d397a909e8a88598
Junou Cui <junou_cui@berkeley.edu>
2019-07-07 16:29:13 -0700

    288

---
+++ Java/288__Unique_Word_Abbreviation.java
@@ -0,0 +1,33 @@
+class ValidWordAbbr {
+
+    Map<String, Set<String>> abbrDict = new HashMap<>();
+
+    public ValidWordAbbr(String[] dictionary) {
+        for (String s : dictionary) {
+            String abbr = toAbbr(s);
+            Set<String> words = abbrDict.containsKey(abbr) ? abbrDict.get(abbr) : new HashSet<>();
+            words.add(s);
+            abbrDict.put(abbr, words);
+        }
+    }
+
+    public boolean isUnique(String word) {
+        String abbr = toAbbr(word);
+        Set<String> words = abbrDict.get(abbr);
+        return words == null || (words.size() == 1 && words.contains(word));
+    }
+
+    private String toAbbr(String s) {
+        int n = s.length();
+        if (n <= 2) {
+            return s;
+        }
+        return s.charAt(0) + Integer.toString(n - 2) + s.charAt(n - 1);
+    }
+}
+
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * ValidWordAbbr obj = new ValidWordAbbr(dictionary);
+ * boolean param_1 = obj.isUnique(word);
+ */
