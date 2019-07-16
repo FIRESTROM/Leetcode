b0032ba1db9e9372f7a019b34a565bb45001caee
Junou Cui <junou_cui@berkeley.edu>
2019-07-15 18:42:14 -0700

    384

---
+++ Java/384__Shuffle_an_Array.java
@@ -0,0 +1,34 @@
+class Solution {
+    int[] array;
+    int[] original;
+
+    Random rand = new Random();
+
+    public Solution(int[] nums) {
+        array = nums;
+        original = nums.clone();
+    }
+
+    public int[] reset() {
+        array = original;
+        original = original.clone();
+        return original;
+    }
+
+    public int[] shuffle() {
+        for (int i = 0; i < array.length; i++) {
+            swapAt(i, randRange(i, array.length));
+        }
+        return array;
+    }
+
+    private int randRange(int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private void swapAt(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
